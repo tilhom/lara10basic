2ae5543eb2e53c2dc8d8737879d574313646132d
ilyosxizr <ilhomteshaboyev@gmail.com>
2023-03-22 19:16:51 +0500

    first

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>iTech Test</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+<h2>Hello {{ $data1 }}!</h2>
+</body>
+</html>
