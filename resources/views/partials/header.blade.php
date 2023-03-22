02092623503380fc8823812d8f2d517ebb877d7e
ilyosxizr <ilhomteshaboyev@gmail.com>
2023-03-23 04:02:24 +0500

    login

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -380,7 +380,14 @@
                                 <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
                                 <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item text-danger" href="#"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <a class="dropdown-item text-danger" href="route('logout')" 
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    <i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+                                </form>
                             </div>
                         </div>
 
