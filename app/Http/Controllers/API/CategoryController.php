c2454f1342aef445435ae893f4005ef0e386c83b
ilyosxizr <ilhomteshaboyev@gmail.com>
2023-03-30 13:04:39 +0500

    rest api

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $categories
+        ]);
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $category->toArray()
+        ], 400);
+    }
+
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->category_name = $request->category_name;
+        $category->save();
+
+        return response()->json([
+            'success' => true,
+            'data' => $category->toArray()
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category not found'
+            ], 400);
+        }
+
+        $updated = $category->fill($request->all())->save();
+
+        if ($updated) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category could not be updated'
+            ], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category not found'
+            ], 400);
+        }
+
+        if ($category->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category could not be deleted'
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::controller(CategoryController::class)->group( function() {
+    Route::get('/categories', 'index');
+    Route::post('/categories', 'store');
+    Route::get('/categories/{id}', 'show');
+    Route::put('/categories/{id}', 'update');
+    Route::delete('/categories/{id}', 'destroy');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
