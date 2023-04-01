8c2d965d776a36191fcf70f2257d6d3e33ee0172
ilyosxizr <ilhomteshaboyev@gmail.com>
2023-04-01 11:05:44 +0500

    sanctum

--- app/Http/Controllers/API/Category2Controller.php
+++ app/Http/Controllers/API/Category2Controller.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+
 
 class Category2Controller extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::paginate(2);
+
+        return CategoryResource::collection($categories);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\Category2Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,8 @@ Route::controller(CategoryController::class)->group( function() {
     Route::put('/categories/{id}', 'update');
     Route::delete('/categories/{id}', 'destroy');
 });
+
+Route::get('/categories2', [Category2Controller::class,'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
